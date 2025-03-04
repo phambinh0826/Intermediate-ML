5238b3ede94abde9921bdc2a883b0a6c0ef17af1
BinhPham0826 <ptcb1012588646@gmail.com>
2025-03-04 21:10:11 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,24 +10,33 @@ version = '0.1.0'
 
 # -- General configuration
 extensions = [
+    'nbsphinx',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'nbsphinx',  # Added nbsphinx to handle Jupyter Notebooks
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
+    'sphinx_gallery.load_style',
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
 # -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
+
+master_doc = 'index'
+
+highlight_language = 'python3'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
