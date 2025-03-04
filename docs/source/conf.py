4ef3d9f833b8ff2022c5673359a1166000802d93
BinhPham0826 <ptcb1012588646@gmail.com>
2025-03-04 21:13:36 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,10 @@ version = '0.1.0'
 
 # -- General configuration
 extensions = [
-    'nbsphinx',
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx_rtd_theme',
-    'sphinx_copybutton',
-    'sphinx_gallery.load_style',
+    'sphinx.ext.napoleon',
+    'nbsphinx',  # Hỗ trợ Jupyter Notebook
+    'myst_parser'  # Hỗ trợ Markdown
 ]
 
 intersphinx_mapping = {
